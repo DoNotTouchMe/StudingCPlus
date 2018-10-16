c2016f3393689a485de44ef9a0dcf62993c30a99
DoNotTouchMe <tridognait0@gamil.com>
2018-10-16 20:09:01 +0400

    Eng calc

--- calk/main.cpp
+++ calk/main.cpp
@@ -10,24 +10,24 @@ int main(int argc, const char * argv[]){
     int result;
     string oper;
     string sum = "sum";
-    string subt = "subt";
+    string differ = "differ";
     string multi = "multi";
     string divis = "divis";
 
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-    cout << "Введите число" <<endl;
+    cout << "Insert the number." <<endl;
     cin >> num1;
-    cout << "Введите оператор: sum для суммы, divis для деления, subt для разности или multi для умножения" <<endl;
+    cout << "Enter operator: sum for sum, divis for division, differ for difference or multi for multiplication." <<endl;
     cin >> oper;
-    cout << "Введите второе число" <<endl;
+    cout << "Insert second number." <<endl;
     cin >> num2;
 
     if(oper == sum) {
         result = num1 + num2;
         cout << num1 << " + " << num2 << " = "<< result <<endl;
     }
-    else if (oper == subt) {
+    else if (oper == differ) {
         result = num1 - num2;
         cout << num1 << " - " << num2 << " = " << result <<endl;
     }
