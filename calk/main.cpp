2e1519ebded9760f29bfda773e9b97d70aaf19d6
DoNotTouchMe <tridognait0@gamil.com>
2018-10-18 21:09:40 +0400

    wcout

--- calk/main.cpp
+++ calk/main.cpp
@@ -13,37 +13,37 @@ int main(int argc, const char * argv[]){
     string multi = "multi";
     string divis = "divis";
 
-    cout << "Insert the number." <<endl;
+    wcout << "Insert the number." <<endl;
     cin >> num1;
-    cout << "Enter operator: sum for sum, divis for division, differ for difference or multi for multiplication." <<endl;
+    wcout << "Enter operator: sum for sum, divis for division, differ for difference or multi for multiplication." <<endl;
     cin >> oper;
-    cout << "Insert second number." <<endl;
+    wcout << "Insert second number." <<endl;
     cin >> num2;
 
     if(oper == sum) {
         result = num1 + num2;
-        cout << num1 << " + " << num2 << " = "<< result <<endl;
+        wcout << num1 << " + " << num2 << " = "<< result <<endl;
     }
     else if (oper == differ) {
         result = num1 - num2;
-        cout << num1 << " - " << num2 << " = " << result <<endl;
+        wcout << num1 << " - " << num2 << " = " << result <<endl;
     }
     else if(oper == multi) {
         result = num1 * num2;
-        cout << num1 << " * " << num2 << " = " << result <<endl;
+        wcout << num1 << " * " << num2 << " = " << result <<endl;
     }
     else if(oper == divis) {
         try{
             if(num2 == 0)
             throw 123;
             result = num1 / num2;
-            cout << num1 << " / " << num2 << " = " << result <<endl;
+            wcout << num1 << " / " << num2 << " = " << result <<endl;
         } catch (int i){
-            cout << "Error #" << i << " divide by 0!" << endl;
+            wcout << "Error #" << i << " divide by 0!" << endl;
         }
     }
     else{
-        cout << "Result is unknown" <<endl;
+        wcout << "Result is unknown" <<endl;
     }
 
     system("pause");
